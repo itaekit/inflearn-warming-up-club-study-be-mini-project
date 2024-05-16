f5c12c8cda65bc6165935ae4d1519f1233c5b4e2
Yongtaek Lee <itaekit@gmail.com>
2024-05-17 08:34:58 +0900

    initial commit

---
+++ src/main/java/com/warmingup/inhouseapp/InhouseAppApplication.java
@@ -0,0 +1,13 @@
+package com.warmingup.inhouseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhouseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InhouseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/warmingup/inhouseapp/InhouseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warmingup.inhouseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhouseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
